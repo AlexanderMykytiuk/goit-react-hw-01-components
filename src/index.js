72ea5f283450551ac2e865d0e8f7e9afa03842bb
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-02-16 15:36:00 +0200

    Налаштував React

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const user = {
+  "name": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+}
