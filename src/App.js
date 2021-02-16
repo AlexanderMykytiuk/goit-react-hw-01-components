a5619ff855a9aed9edec52a33f98b896ddaa1f4f
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-02-16 17:31:48 +0200

    Додав App, Profile, user.json

--- src/App.js
+++ src/App.js
@@ -1,2 +1,21 @@
+import React from "react";
+import Profile from "./components/Profile";
+import user from './user.json'
 
+const App = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <Profile
+        name ="Jacques Gluke"
+       tag ="jgluke"
+       location ="Ocho Rios, Jamaica"
+       avatar ="https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg"
+         followers = {5603}
+         views = {4827}
+         likes = {1308}
+      />
+    </div>
+  );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const user = {
-  "name": "Jacques Gluke",
-  "tag": "jgluke",
-  "location": "Ocho Rios, Jamaica",
-  "avatar": "https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg",
-  "stats": {
-    "followers": 5603,
-    "views": 4827,
-    "likes": 1308
-  }
-}
+
+
+
+// <Profile
+//   name={user.name}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />
