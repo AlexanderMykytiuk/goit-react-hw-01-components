886a0b88eda219b2412c0f9fae0f46c5461f27f7
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-02-17 20:41:56 +0200

    Розпочав 3 завдання

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Profile from "./components/Profile";
-import user from "./user.json";
-import statisticalData from "./statistical-data.json";
+import user from "./data/user.json";
+
+import statisticalData from "./data/statistical-data.json";
 import Statistics from "./components/Statistics";
+
+import friends from "./data/friends.json";
+import FriendList from "./components/FriendList";
 
 const App = () => {
   return (
@@ -14,8 +18,12 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
+
+      <FriendList friends={friends} />
+      
     </div>
   );
 };
