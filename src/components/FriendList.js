b7c2d757c33d95b9efb1971689b15133121fd066
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-02-18 15:48:53 +0200

    Виконав 3 задачу без стилізації

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,2 +1,29 @@
 import React from "react";
-import propTypes from 'prop-types';
+import propTypes from "prop-types";
+import FriendListItem from "./FriendListItem";
+
+const FriendList = ({ friends }) => (
+  <ul className="friend-list">
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <FriendListItem
+        isOnline={isOnline}
+        avatar={avatar}
+        name={name}
+        key={id}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: propTypes.arrayOf(
+    propTypes.shape({
+      avatar: propTypes.string.isRequired,
+      name: propTypes.string.isRequired,
+      isOnline: propTypes.bool.isRequired,
+      id: propTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
