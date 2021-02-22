18bdde1c67eeef24a60b578147282eb877368144
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-02-22 17:30:16 +0200

    Стилізую завдання 2

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styles from "./Statistcs.module.css";
 
 
 const Statistics = ({ title, stats }) => (
-  <section className="statistics">
-    {title && <h2 className="title">{title}</h2>}
+  <section className={styles.statistics}>
+    {title && <h2 className={styles.title}>{title}</h2>}
     
-    <ul className="stat-list">
+    <ul className={styles.stat_list}>
       {stats.map(({ id, label, percentage }) => (
-        <li className="item" key={id}>
-          <span className="label">{label} </span>
-          <span className="percentage">{percentage}%</span>
+        <li className={styles.item} key={id}>
+          <span className={styles.label}>{label} </span>
+          <span className = {styles.percentage}>{percentage}%</span>
         </li>
       ))}
     </ul>
@@ -31,22 +32,3 @@ Statistics.propTypes = {
 }
 
 export default Statistics;
-
-// const Statistics = ({ statistic}) => (
-//   <section className={styles.statistics}>
-//     <h2>Задание №2</h2>
-//     <h2 className={styles.title}>Upload stats</h2>
-//     <ul>
-//       {statistic.map(({ id, label, percentage }, index) => (
-//         <li
-//           className={styles.item}
-//           key={id}
-//           style={{ backgroundColor: colors[index] }}
-//         >
-//           <span className={styles.label}>{label}</span>
-//           <span className={styles.percentage}>{percentage}%</span>
-//         </li>
-//       ))}
-//     </ul>
-//   </section>
-// );
