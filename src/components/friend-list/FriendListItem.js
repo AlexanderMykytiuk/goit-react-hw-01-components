8f75010ac362b28a7e6f7565e8b376776a33aa91
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-02-22 20:00:11 +0200

    Виконав ДЗ №1

--- src/components/friend-list/FriendListItem.js
+++ src/components/friend-list/FriendListItem.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import styles from "./Friends.module.css"
+import React from "react";
+import styles from "./Friends.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline, id }) => (
-    <li className={styles.item} key={id}>
-        <span className={styles.status}>{isOnline}</span>
-        <img className={styles.avatar} src={avatar} alt={name} width="48" />
-        <p className={styles.name}>{name}</p>
-    </li>
-);  
+  <li className={styles.item} key={id}>
+    <span className={styles.status}>{isOnline}</span>
+    {isOnline ? (
+      <span className={styles.isOnline}>{isOnline}</span>
+    ) : (
+      <span className={styles.isOffline}>{isOnline}</span>
+    )}
+
+    <img className={styles.avatar} src={avatar} alt={name} width="48" />
+    <p className={styles.name}>{name}</p>
+  </li>
+);
 
 export default FriendListItem;
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React from "react";
+import propTypes from "prop-types";
 import styles from "./Statistcs.module.css";
 
+const colors = ["blueviolet", "palevioletred", "orange", "royalblue", "greenyellow"];
 
 const Statistics = ({ title, stats }) => (
   <section className={styles.statistics}>
     {title && <h2 className={styles.title}>{title}</h2>}
-    
+
     <ul className={styles.stat_list}>
-      {stats.map(({ id, label, percentage }) => (
-        <li className={styles.item} key={id}>
-          <span className={styles.label}>{label} </span>
-          <span className = {styles.percentage}>{percentage}%</span>
+      {stats.map(({ id, label, percentage }, index) => (
+        <li
+          className={styles.item}
+          key={id}
+          style={{ backgroundColor: colors[index] }}
+        >
+          <span className={styles.label}>{label}</span>
+          <span className={styles.percentage}>{percentage}%</span>
         </li>
       ))}
     </ul>
@@ -19,16 +24,18 @@ const Statistics = ({ title, stats }) => (
 );
 
 Statistics.defaultProps = {
-  title: '',
+  title: "",
 };
 
 Statistics.propTypes = {
   title: propTypes.string,
-  stats: propTypes.arrayOf(propTypes.shape({
-    id: propTypes.string.isRequired,
-    label: propTypes.string.isRequired,
-    percentage: propTypes.number.isRequired,
-  })).isRequired
-}
+  stats: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      label: propTypes.string.isRequired,
+      percentage: propTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default Statistics;
